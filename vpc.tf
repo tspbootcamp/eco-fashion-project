7b1a22e3036f4cd603df34c9005efc557018a727
charlino <charlino4u@gmail.com>
2025-07-02 03:14:26 +0000

    updated k8s,and terraform regij

--- vpc.tf
+++ vpc.tf
@@ -9,10 +9,10 @@
 resource "aws_vpc" "tsp-vpc" {
   cidr_block = "10.0.0.0/16"
 
-  tags = tomap({
-    "Name" = "tsp-eks-node",
-    "kubernetes.io/cluster/${var.cluster-name}" = "shared",
-  })
+  tags = {
+    Name                                      = "tsp-eks-node"
+    "kubernetes.io/cluster/${var.cluster-name}" = "shared"
+  }
 }
 
 resource "aws_subnet" "tsp-vpc" {
@@ -23,10 +23,11 @@ resource "aws_subnet" "tsp-vpc" {
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.tsp-vpc.id
 
-  tags = tomap({
-    "Name" = "tsp-eks-node",
-    "kubernetes.io/cluster/${var.cluster-name}" = "shared",
-  })
+  tags = {
+    Name = "tsp-eks-node-${count.index}"
+    "kubernetes.io/cluster/${var.cluster-name != "" ? var.cluster-name : "tsp-cluster-${terraform.workspace}"}" = "shared"
+    "kubernetes.io/role/elb" = "1"  # Needed for public ELB access
+  }
 }
 
 resource "aws_internet_gateway" "tsp-vpc" {
@@ -44,11 +45,15 @@ resource "aws_route_table" "tsp-vpc" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.tsp-vpc.id
   }
+
+  tags = {
+    Name = "tsp-vpc-route"
+  }
 }
 
 resource "aws_route_table_association" "tsp-vpc" {
   count = 2
 
-  subnet_id      = aws_subnet.tsp-vpc.*.id[count.index]
+  subnet_id      = aws_subnet.tsp-vpc[count.index].id
   route_table_id = aws_route_table.tsp-vpc.id
 }
